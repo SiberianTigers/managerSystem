6c09b6a645b85412438495b80bde9c4321ea2d52
lxc688 <237024898@qq.com>
2018-12-29 13:35:25 +0800

    提交测试

---
+++ jubao-manager/jubao-manager-web/src/main/java/com/jubao/controller/Demo1.java
@@ -0,0 +1,16 @@
+package com.jubao.controller;
+
+/***
+ *   IDEA   git 测试
+ */
+public class Demo1 {
+
+    public static void main(String [] args){
+
+
+
+
+        System.out.println("-----------git------------------------");
+    }
+
+}
